a252d3ae942558318b3f3063b7cb4e12f3945b75
tarun dixit <dixittarun81@gmail.com>
2019-08-07 00:42:19 +0530

    LogoutFeature

--- app/src/main/java/com/example/firebaseblog/MainActivity.java
+++ app/src/main/java/com/example/firebaseblog/MainActivity.java
@@ -3,15 +3,78 @@ package com.example.firebaseblog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mainToolbar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-        startActivity(intent);
+        mAuth = FirebaseAuth.getInstance();
+
+        mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
+        getSupportActionBar().setTitle("Photo Blog");
+
+
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentUser == null){
+            sendTologin();
+        }
+        else{
+
+        }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout_btn:
+                logout();
+                return true;
+
+            default:
+
+                
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        mAuth.signOut();
+        sendTologin();
+    }
+
+    private void sendTologin() {
+        Intent mainIntent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(mainIntent);
+        finish();
     }
 }
